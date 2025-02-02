dc7d7a5c1d23a4b7a24c6ca28acaffba0b93766c
VimBotion <sucoandchino@gmail.com>
2025-02-02 09:41:08 -0500

    fix

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,9 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/snacks.nvim/", opts = {
-    scroll = { enabled = false },
-    dim = { enabled = false },
-  } },
   {
     "folke/snacks.nvim",
     opts = {
